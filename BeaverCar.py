c3b250d35bbf48b21ce65ce712ef14aeb4c9e0e7
eSqadron <kubam0906@gmail.com>
2021-11-06 17:50:05 +0100

    InitCommit

---
+++ BeaverCar.py
@@ -0,0 +1,6 @@
+class BeaverCar:
+    def __init__(self):
+        pass
+
+    def runCar(self):
+        pass
---
+++ main.py
@@ -0,0 +1,5 @@
+from BeaverCar import BeaverCar
+
+if __name__ == '__main__':
+    car = BeaverCar()
+    car.runCar()
